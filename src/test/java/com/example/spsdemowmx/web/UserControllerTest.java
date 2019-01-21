2d86f7e4ea9cfd5e0182cf12565278561534501b
吴梦萱 <wumengxuan@cn.wilmar-intl.com>
2019-01-21 13:01:34 +0800

    Add UserControllerTest

--- src/test/java/com/example/spsdemowmx/web/UserControllerTest.java
+++ src/test/java/com/example/spsdemowmx/web/UserControllerTest.java
@@ -1,11 +1,47 @@
 package com.example.spsdemowmx.web;
 
 
+import com.example.spsdemowmx.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserControllerTest {
+    @Autowired
+    WebApplicationContext context;
+    @Autowired UserController userController;
+    @Autowired UserRepository userRepository;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void findAll(){
+        assertEquals(3, userRepository.findAll().size());
+    }
+
+    @Test
+    public void mockFindAll() throws Exception{
+        mvc.perform(get("/api/users")).andExpect(status().isOk()).andDo(print());
+    }
+
 }
